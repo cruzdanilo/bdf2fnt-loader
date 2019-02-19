f12f4b5720a0754cd451642f7cf1f129a8cac3f5
danilo neves cruz <cruzdanilo@gmail.com>
2019-02-18 23:18:38 -0300

    migrate from jimp to sharp

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
 const path = require('path');
 const loaderUtils = require('loader-utils');
 const BDF = require('bdfjs');
-const Jimp = require('jimp');
 const imagemin = require('imagemin');
 const optipng = require('imagemin-optipng');
+const sharp = require('sharp');
 
 const done = new Set();
 
-module.exports = function loader(content) {
+module.exports = async function loader(content) {
+  this.async();
   const options = loaderUtils.getOptions(this) || {};
   const font = BDF.parse(content);
   const charset = new Set([...(options.charset || ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.0123456789')]);
-  const box = font.meta.boundingBox;
-  const side = 2 ** Math.ceil(Math.log2(Math.sqrt(charset.size * box.width * box.height)));
-  const image = new Jimp(side, side);
+  const { width: charWidth, height: charHeight, y: charY } = font.meta.boundingBox;
+  const width = 2 ** Math.ceil(Math.log2(Math.sqrt(charset.size * charWidth * charHeight)));
+  const columns = Math.floor(width / charWidth);
+  const height = Math.ceil(charset.size / columns) * charHeight;
+  const channels = 4;
   const chars = [];
-  let x = 0;
-  let y = 0;
-  const baseline = box.height + box.y;
-  charset.forEach((char) => {
+  const pixels = Buffer.alloc(width * height * channels);
+  [...charset].forEach((char, i) => {
     const glyph = font.glyphs[char.charCodeAt()];
-    const rowStart = baseline - glyph.boundingBox.y - glyph.boundingBox.height;
-    glyph.bitmap.forEach((row, i) => row.forEach((value, j) => {
-      if (value) image.setPixelColor(0xffffffff, x + glyph.boundingBox.x + j, y + rowStart + i);
-    }));
+    const x = (i % columns) * charWidth;
+    const y = Math.floor(i / columns) * charHeight;
     chars.push({ id: glyph.code, x, y });
-    x += box.width;
-    if (x + box.width > image.bitmap.width) {
-      x = 0;
-      y += box.height;
-    }
+    const { boundingBox: box } = glyph;
+    const firstColumn = x + box.x;
+    const firstRow = y + charHeight + charY - box.height - box.y;
+    glyph.bitmap.forEach((row, r) => row.forEach((value, c) => {
+      if (!value) return;
+      const offset = ((firstRow + r) * width + firstColumn + c) * channels;
+      pixels.fill(0xff, offset, offset + channels);
+    }));
   });
-  this.async();
-  image.getBuffer(Jimp.MIME_PNG, (err, buf) => {
-    if (err) {
-      this.callback(err);
-      return;
-    }
-    imagemin.buffer(buf, { use: [optipng()] }).then((png) => {
-      const pngName = loaderUtils.interpolateName(this, '[name].[hash:8].png', { content: png });
-      const outputPath = options.outputPath || '';
-      let fnt = `common lineHeight=${box.height} base=${box.height} scaleW=${image.bitmap.width} scaleH=${image.bitmap.height} pages=1\n`;
-      fnt += `page id=0 file="${pngName}"\n`;
-      fnt += `chars count=${chars.length}\n`;
-      chars.forEach((char) => {
-        fnt += `char id=${char.id} x=${char.x} y=${char.y} width=${box.width} height=${box.height} xoffset=0 yoffset=0 xadvance=${box.width} page=0 \n`;
-      });
-      const fntName = path.posix.join(outputPath, loaderUtils.interpolateName(this, '[name].[hash:8].fnt', { content: fnt }));
-      if (!done.has(pngName)) {
-        done.add(pngName);
-        this.emitFile(path.posix.join(outputPath, pngName), png);
-        this.emitFile(fntName, fnt);
-      }
-      this.callback(null, `export default __webpack_public_path__ + ${JSON.stringify(fntName)};`);
-    }).catch(e => this.callback(e));
+  const png = await imagemin.buffer(
+    await sharp(pixels, { raw: { width, height, channels } }).png().toBuffer(),
+    { use: [optipng()] },
+  );
+  const pngName = loaderUtils.interpolateName(this, options.name || '[name].[hash:8].png', { content: png });
+  const outputPath = options.outputPath || '';
+  let fnt = `common lineHeight=${charHeight} base=${charHeight + charY} scaleW=${width} scaleH=${height} pages=1\n`;
+  fnt += `page id=0 file="${pngName}"\n`;
+  fnt += `chars count=${chars.length}\n`;
+  chars.forEach((char) => {
+    fnt += `char id=${char.id} x=${char.x} y=${char.y} width=${charWidth} height=${charHeight} xoffset=0 yoffset=0 xadvance=${charWidth} page=0 \n`;
   });
+  const fntName = path.posix.join(outputPath, loaderUtils.interpolateName(this, '[name].[hash:8].fnt', { content: fnt }));
+  if (!done.has(pngName)) {
+    done.add(pngName);
+    this.emitFile(path.posix.join(outputPath, pngName), png);
+    this.emitFile(fntName, fnt);
+  }
+  this.callback(null, `export default __webpack_public_path__ + ${JSON.stringify(fntName)};`);
 };
 
 module.exports.raw = true;
