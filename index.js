979838bc2e4aee00b46999ee93b246551b445e12
danilo neves cruz <cruzdanilo@gmail.com>
2020-06-26 00:25:32 +0700

    generate webp textures, improve optimization

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const path = require('path');
-const loaderUtils = require('loader-utils');
-const BDF = require('bdfjs');
-const imagemin = require('imagemin');
-const optipng = require('imagemin-optipng');
+const { join, parse: parsePath, relative } = require('path').posix;
+const { getOptions, interpolateName } = require('loader-utils');
+const { create } = require('xmlbuilder2');
+const { parse } = require('bdfjs');
+const optipng = require('imagemin-optipng')();
 const sharp = require('sharp');
-const xmlbuilder = require('xmlbuilder');
 
-const done = new Set();
+const build = async (content, context) => {
+  const {
+    webp = true,
+    prettyPrint = false,
+    name = '[name].[contenthash:8].[ext]',
+    outputPath = relative(context.rootContext, context.context),
+    charset: charsetString = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.0123456789',
+  } = getOptions(context) || {};
+  const { resourcePath } = context;
 
-module.exports = async function loader(content) {
-  this.async();
-  const options = loaderUtils.getOptions(this) || {};
-  const outputPath = options.outputPath || path.posix.relative(this.rootContext, this.context);
-  const font = BDF.parse(content);
-  const charset = new Set([...(options.charset || ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.0123456789')]);
+  const font = parse(content);
   const { width: charWidth, height: charHeight, y: charY } = font.meta.boundingBox;
   const width = 2 ** Math.ceil(Math.log2(Math.sqrt(charset.size * charWidth * charHeight)));
   const columns = Math.floor(width / charWidth);
   const height = Math.ceil(charset.size / columns) * charHeight;
   const channels = 4;
+  const charset = new Set([...charsetString]);
   const chars = [];
   const pixels = Buffer.alloc(width * height * channels);
   [...charset].forEach((char, i) => {
@@ -35,40 +38,56 @@ module.exports = async function loader(content) {
       pixels.fill(0xff, offset, offset + channels);
     }));
   });
-  const texture = await imagemin.buffer(
-    await sharp(pixels, { raw: { width, height, channels } }).png().toBuffer(),
-    { use: [optipng()] },
-  );
-  const name = options.name || '[name].[contenthash:8].[ext]';
-  const textureName = loaderUtils.interpolateName(this, {
-    ...this, resourcePath: this.resourcePath.replace(/\.bdf(?!.*\.bdf)/, '.png'),
-  }, { content: texture });
-  const fontData = chars.reduce(
+
+  const sharpline = sharp(pixels, { raw: { width, height, channels } });
+  const textures = await Promise.all([
+    ['.png', optipng, sharpline.clone().png({ compressionLevel: 0 })],
+    ...webp ? [
+      ['.webp', (b) => b, sharpline.clone().webp({ quality: 100, lossless: true, reductionEffort: 6 })],
+    ] : [],
+  ].map(async ([ext, optimizer, pipeline]) => {
+    const data = await optimizer(await pipeline.toBuffer());
+    const filepath = join(outputPath, interpolateName({
+      ...context, resourcePath: resourcePath.replace(/\.bdf(?!.*\.bdf)/, ext),
+    }, name, { content: data }));
+    context.emitFile(filepath, data);
+    return filepath;
+  }));
+
+  const fontDataBuffer = chars.reduce(
     (xml, char) => {
-      xml.ele('char', { id: char.id, x: char.x, y: char.y, width: charWidth, height: charHeight, xoffset: 0, yoffset: 0, xadvance: charWidth, page: 0 }); // eslint-disable-line object-curly-newline
+      xml.ele('char', { id: char.id, x: char.x, y: 0, width: char.width, height, xoffset: 0, yoffset: 0, xadvance: char.width + 1, page: 0 }); // eslint-disable-line object-curly-newline
       return xml;
     },
-    xmlbuilder.create('font')
-      .ele('info', { face: font.meta.name, size: font.meta.size.points })
+    create().ele('font')
+      .ele('info', { face: parsePath(resourcePath).name.split('.bdf')[0], size: height })
       .up()
-      .ele('common', { lineHeight: charHeight, base: charHeight + charY, scaleW: width, scaleH: height, pages: 1 }) // eslint-disable-line object-curly-newline
+      .ele('common', { lineHeight: height, base: height, scaleW: width, scaleH: height, pages: 1 }) // eslint-disable-line object-curly-newline
       .up()
       .ele('pages')
-      .ele('page', { id: 0, file: textureName })
+      .ele('page', { id: 0, file: relative(outputPath, textures[0]) })
       .up()
       .up()
       .ele('chars', { count: chars.length }),
-  ).end({ pretty: true });
-  const texturePath = path.posix.join(outputPath, textureName);
-  const fontDataPath = path.posix.join(outputPath, loaderUtils.interpolateName({
-    ...this, resourcePath: this.resourcePath.replace(/\.bdf(?!.*\.bdf)/, '.xml'),
-  }, name, { content: fontData }));
-  if (!done.has(textureName)) {
-    done.add(textureName);
-    this.emitFile(texturePath, texture);
-    this.emitFile(fontDataPath, fontData);
-  }
-  this.callback(null, `export default { texture: __webpack_public_path__ + ${JSON.stringify(texturePath)}, fontData: __webpack_public_path__ + ${JSON.stringify(fontDataPath)} };`);
+  ).end({ prettyPrint });
+  const fontData = join(outputPath, interpolateName({
+    ...context, resourcePath: resourcePath.replace(/\.bdf(?!.*\.bdf)/, '.xml'),
+  }, name, { content: fontDataBuffer }));
+  context.emitFile(fontData, fontDataBuffer);
+  return { fontData, textures };
+};
+
+const done = new Map();
+
+module.exports = async function loader(content) {
+  this.async();
+  const contenthash = interpolateName(this, '[contenthash]', { content });
+  if (!done.has(contenthash)) done.set(contenthash, await build(content, this));
+  const { fontData, textures } = done.get(contenthash);
+  this.callback(null, `export default {
+  fontData: __webpack_public_path__ + ${JSON.stringify(fontData)},
+  textures: [${textures.map((t) => `__webpack_public_path__ + ${JSON.stringify(t)}`).join(', ')}],
+};`);
 };
 
 module.exports.raw = true;
